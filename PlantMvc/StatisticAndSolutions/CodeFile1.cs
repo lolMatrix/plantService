5302bc2405d0af2aa654ffc0c827f6965ed871d5
MihaniaMORG <berfeg228@gmail.com>
2021-07-06 23:50:42 +0300

    create class instrument

---
+++ PlantMvc/StatisticAndSolutions/CodeFile1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using DataBase;
+using Entities.Models;
+
+namespace PlantService
+{
+    public class Instruments
+    {
+        public string name;
+        public int value;
+    }
+    
+}
