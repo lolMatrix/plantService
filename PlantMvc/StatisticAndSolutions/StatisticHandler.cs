2df4fdef2372446eab1123f4fa7179b44189a4dd
MihaniaMORG <berfeg228@gmail.com>
2021-07-11 02:59:34 +0300

    Create interface body
    kringe

---
+++ PlantMvc/StatisticAndSolutions/StatisticHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using DataBase;
+using Entities.Enums;
+using System.Collections.Generic;
+using Entities.Models;
+
+
+
+namespace StatisticAndSolutions
+{
+    public class Program
+    {
+        SortedList<Sensor, int[]> DataSave = new SortedList<Sensor, int[]>();
+    }
+    public class StatisticHandler
+    {
+        
+        public void ReturnList(SortedList<Sensor.Name,int[]> DataSave)
+        {
+            
+        }
+    }
+}
