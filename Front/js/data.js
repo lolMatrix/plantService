34d25af427af967f0d276ccb293e8632d620bc76
lolMatrix <dr.lesha@live.com>
2021-07-15 20:02:05 +0300

    [fix] merge [feat] charts [feat] config file

--- Front/js/data.js
+++ Front/js/data.js
@@ -13,6 +13,21 @@ function getDataFromServer(url) {
     return data;
 }
 
+function getType(type){
+    switch (type){
+        case "WaterSensor":
+            type = "Сенсор влажности почвы";
+            break;
+        case "AirSensor":
+            type = "Сенсор воздуха";
+            break;
+        case "TemperatureSensor":
+            type = "Сенсор температуры";
+            break;
+    }
+    return type;
+}
+
 function getDetails(url, id, lambda){
     var details = getDataFromServer(url + id);
 
@@ -38,21 +53,65 @@ function getList(url, id, lambda){
     });
 }
 
-function getGraph(url, id, lambda){
-    var graph = getDataFromServer(url + id);
-    
-    graph.forEach(element => {
-        var array = lambda(element);
-        var html = $('<div>').load("htmlTemplates/graph.html", function(){
-            //html.find(".title").text(array[0]);
-           // html.find(".content").text(array[1]);
-           // html.find(".card").attr("onclick", array[2]);
-        });
-        
-        $("#content").append(html);
-    });
+function getDataForMounth(id){
+    var first = new Date();
+    first.setMonth(first.getMonth() - 1);
+    first = first.toISOString();
 
-   
+    var url = getHost() + 'data/get?sensorId=' + id;
+    url += '&first=' + first;
     
+    var second = new Date();
+    url += '&second=' + second.toISOString();
+    var data = getDataFromServer(url);
+    data.sort(function(a, b) {
+        Date.parse(a.timeMeasurement) - Date.parse(b.timeMeasurement);
+    });
+    return data;
 }
 
+function getDate(date) {
+    return Date.parse(date);
+}
+
+function parseData(data){
+    var array = [];
+    data.forEach(element => {
+            var object = new Object();
+            object.x = Date.parse(element.timeMeasurement);
+            object.y = element.value;
+            array.push(object);
+    });
+    return array;
+}
+
+function drawChart(data, title, alias){
+    var data = parseData(data);
+    $("#chart").shieldChart({
+            theme: "light",
+            primaryHeader: {
+            text: title
+            },
+            exportOptions: {
+                    image: false,
+                    print: false
+            },
+            tooltipSettings: {
+                    chartBound: true,
+                    axisMarkers: {
+                            enabled: true,
+                            mode: 'xy'
+                    } 
+            },
+            axisX: {
+                    axisType: 'datetime'
+            },
+            dataSeries: [
+                    {
+                            seriesType: 'line',
+                            collectionAlias: alias,
+                            data: data 
+                    }
+            ]
+    });
+}       
--- Front/js/main.js
+++ Front/js/main.js
@@ -1,46 +1,64 @@
 $(document).ready(function (){
     getGreenhoseList();
-})
+});
+
 function getGreenhoseList(){
-    var greenhose = getDataFromServer("https://localhost:44359/greenhose");
+    var greenhose = getDataFromServer(getHost() + "greenhose");
     greenhose.forEach(element => {
         var html = $('<div>').load("htmlTemplates/data.html", function(){
             html.find(".title").text(element.name);
             html.find(".content").text(element.location);
             html.find(".card").attr("onclick", "getBedsList(" + element.id + ")");
         });
-    $("#content").append(html);
+
+        $("#content").append(html);
     });
 }
 
 function getBedsList(id){
     $("#content").empty();
-    getDetails("https://localhost:44359/greenhose/", id, function(data){
+    getDetails(getHost() + "greenhose/", id, function(data){
        return [data.name, data.location];
     });
-    getList("https://localhost:44359/bed/get/", id, function(data){
+    getList(getHost() + "bed/get/", id, function(data){
         return [data.name, 'Кол-во воды в баке: ' + data.waterVolume + ' литров.', 'getSensorList(' + data.id + ')'];
     })
 }
 
 function getSensorList(id){
     $("#content").empty();
-    getDetails("https://localhost:44359/bed/", id, function (data){
+    getDetails(getHost() + "bed/", id, function (data){
         return [data.name, 'Кол-во воды в баке: ' + data.waterVolume + ' литров.'];
     });
-    getList("https://localhost:44359/sensor/get/", id, function(data){
-        var type = "";
-        switch (data.type){
-            case "WaterSensor":
-                type = "Сенсор влажности почвы";
-                break;
-            case "AirSensor":
-                type = "Сенсор воздуха";
-                break;
-            case "TemperatureSensor":
-                type = "Сенсор температуры";
-                break;
-        }
-        return [data.name, type, 'getCharts(' + data.id + ')'];
+    getList(getHost() + "sensor/get/", id, function(data){
+        return [data.name, getType(data.type), 'getSensorDetails(' + data.id + ')'];
     });
+}
+
+function getSensorDetails(id){
+    $('#content').empty();
+
+    var sensorType;
+
+    getDetails(getHost() + "sensor/", id, function(data){
+        sensorType = getType(data.type);
+        return [data.name, sensorType];
+    });
+
+    var data = getDataForMounth(id);
+
+    var html = $('<div>').load("htmlTemplates/graph.html", function(){
+        drawChart(data, sensorType, getMeseasurementUnit(data));
+    });
+    $("#content").append(html);
+
+}
+
+function getMeseasurementUnit(data){
+    var unit = '';
+    if (data.length > 0){
+        unit = data[0].meseasurementUnit;
+    }
+
+    return unit;
 }
