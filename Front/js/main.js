e1a60a44f0fc36fec3b506d0b7e33420a93e1cf4
MatriX <dr.lesha@live.com>
2021-07-16 11:29:58 +0300

    [add] navigation (back button)

--- Front/js/main.js
+++ Front/js/main.js
@@ -1,77 +1,60 @@
 $(document).ready(function (){
     getGreenhoseList();
-});
-
+})
 function getGreenhoseList(){
-    $('title').text("Список теплиц");
-    var greenhose = getDataFromServer(getHost() + "greenhose");
+    $('#content').empty();
+    disableNavigation();
+    var greenhose = getDataFromServer("https://localhost:44359/greenhose");
     greenhose.forEach(element => {
         var html = $('<div>').load("htmlTemplates/data.html", function(){
             html.find(".title").text(element.name);
             html.find(".content").text(element.location);
             html.find(".card").attr("onclick", "getBedsList(" + element.id + ")");
-            html.find(".card").hover(function (){
-                console.log("hovered")
-            });
         });
-
-        $("#content").append(html);
+    $("#content").append(html);
     });
 }
 
 function getBedsList(id){
+    enableNavigation('getGreenhoseList', '');
     $("#content").empty();
-    getDetails(getHost() + "greenhose/", id, function(data){
-        return [data.name, data.location];
+    getDetails("https://localhost:44359/greenhose/", id, function(data){
+       return [data.name, data.location];
     });
-    getList(getHost() + "bed/get/", id, function(data){
+    getList("https://localhost:44359/bed/get/", id, function(data){
         return [data.name, 'Кол-во воды в баке: ' + data.waterVolume + ' литров.', 'getSensorList(' + data.id + ')'];
     })
 }
 
 function getSensorList(id){
     $("#content").empty();
-    getDetails(getHost() + "bed/", id, function (data){
+    getDetails("https://localhost:44359/bed/", id, function (data){
+        enableNavigation('getBedsList', data.bedId);
         return [data.name, 'Кол-во воды в баке: ' + data.waterVolume + ' литров.'];
     });
-    getList(getHost() + "sensor/get/", id, function(data){
-        return [data.name, getType(data.type), 'getSensorDetails(' + data.id + ')'];
+    getList("https://localhost:44359/sensor/get/", id, function(data){
+        var type = "";
+        switch (data.type){
+            case "WaterSensor":
+                type = "Сенсор влажности почвы";
+                break;
+            case "AirSensor":
+                type = "Сенсор воздуха";
+                break;
+            case "TemperatureSensor":
+                type = "Сенсор температуры";
+                break;
+        }
+        return [data.name, type, 'getCharts(' + data.id + ')'];
     });
 }
 
-function getSensorDetails(id){
-    $('#content').empty();
-
-    var sensorType;
-
-    getDetails(getHost() + "sensor/", id, function(data){
-        sensorType = getType(data.type);
-        return [data.name, sensorType];
-    });
-
-    var data = getDataForMounth(id);
-
-    var html;
-    if (data != null){
-        html = $('<div>').load("htmlTemplates/graph.html", function(){
-            drawChart(data, sensorType, getMeseasurementUnit(data));
-        });
-    } else {
-        html = $('<div>').load("htmlTemplates/data.html", function(){
-            html.find(".title").text("Пока нет данных для этого датчика");
-            html.find(".content").text("Но, его можно настроить. Индентификатор датчика: " + id);
-        });
-    }
-
-    $("#content").append(html);
-
+function enableNavigation(backFunction, id){
+    $('#navItem').removeClass('disabled');
+    $('#navItem').attr('onclick', backFunction + '(' + id + ')');
 }
 
-function getMeseasurementUnit(data){
-    var unit = '';
-    if (data.length > 0){
-        unit = data[0].meseasurementUnit;
-    }
-
-    return unit;
+function disableNavigation() {
+    $('#navItem').addClass('disabled');
+    $('#navItem').attr('onclick', '');
 }
